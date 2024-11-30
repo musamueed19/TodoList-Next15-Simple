05273022a90fc6699a054edc78eba1e58ae374a6
Muhammad Musa Mueed <musavuswh@gmail.com>
2024-11-30 12:28:58 +0500

    added heading of todo list form

--- todolist-next15-simple/src/components/TodoForm.jsx
+++ todolist-next15-simple/src/components/TodoForm.jsx
@@ -4,33 +4,34 @@ import Button from "./Button";
 
 const TodoForm = () => {
   return (
-    <form className="bg-slate-900 rounded-sm">
-      <div className="w-[90%] mx-auto py-10">
+    <form className="bg-slate-900 rounded-sm py-8">
+      <h1 className="text-center mb-5 text-3xl text-blue-400 font-bold">Add New List</h1>
+      <div className="w-[90%] mx-auto">
         {/* Form Input Fields - Group Container */}
 
         <div className="space-y-10">
           {/* Todo List name - title Section */}
-          <div className="flex flex-col w-full md:w-[85%] space-y-2">
+          <div className="flex flex-col space-y-2">
             <label htmlFor="name" className="text-lg">
               List Title:
             </label>
             <input
               name="name"
-              className="text-black outline-none px-2 py-1 rounded-md focus:ring-4 focus:ring-slate-500"
+              className="text-black outline-none px-2 py-1 rounded-md focus:ring-4 focus:ring-slate-500 md:w-[85%]"
               placeholder="List name"
               type="text"
             />
           </div>
 
           {/* Todo List Description */}
-          <div className="flex flex-col w-full md:w-[85%] space-y-2">
+          <div className="flex flex-col space-y-2">
             <label htmlFor="name" className="text-lg">
               List Description:
             </label>
             <textarea
               rows={4}
               name="name"
-              className="text-black outline-none px-2 py-1 rounded-md focus:ring-4 focus:ring-slate-500"
+              className="text-black outline-none px-2 py-1 rounded-md focus:ring-4 focus:ring-slate-500 md:w-[85%]"
               placeholder="Description ..."
               type="text"
             />
